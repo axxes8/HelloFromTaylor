f86cceb51429d3cc0c3d1c1b2a1522851a938cc5
axxes8 <axxes8@gmail.com>
2019-05-15 16:33:38 -0600

    added hello.java

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public void sayHello() {
+        System.out.println("Hello, my name is Taylor");
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.sayHello();
+    }
+}
